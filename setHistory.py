0b32233524d3b4bd868dd21ef242678c2512cbe7
apiusage <apiusage8@gmail.com>
2021-04-06 05:05:30 +0800

    Updates

--- setHistory.py
+++ setHistory.py
@@ -4,45 +4,44 @@ import json
 from datetime import datetime
 import pandas as pd
 import base64
-import time
-import plotly.express as px
 
 def run_setHistory():
-    scrapeAllNumbers = st.checkbox('Scrape 0000 to 9999?')
     numberList = st.text_area("Enter winning numbers list", height=150)
     numberList = filterList(numberList)
-    
-    run_Scraping(scrapeAllNumbers, numberList)
 
-def run_Scraping(scrapeAllNumbers, numberList):
+    showGraph = st.checkbox('Show all graphs')
+
+    run_Scraping(numberList, showGraph)
+
+def run_Scraping(numberList, showGraph):
     try:
         url = 'https://www.singaporepools.com.sg/_layouts/15/FourD/FourDCommon.aspx/Get4DNumberCheckResultsJSON'
         headers = {
-            'Host':'www.singaporepools.com.sg',
-            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
-            'Accept':'application/json, text/javascript, */*; q=0.01',
-            'Accept-Language':'en-US,en;q=0.5',
-            'Accept-Encoding':'gzip, deflate',
-            'Referer':'http://www.singaporepools.com.sg/en/product/Pages/4d_cpwn.aspx',
-            'Content-Type':'application/json',
-            'X-Requested-With':'XMLHttpRequest',
-            'Content-Length':'76'
+            'Host': 'www.singaporepools.com.sg',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate',
+            'Referer': 'http://www.singaporepools.com.sg/en/product/Pages/4d_cpwn.aspx',
+            'Content-Type': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Content-Length': '76'
         }
 
         # Convert milliseconds to local time
         def getDateFromDrawDate(x):
-            x = x.replace('/Date(','')
-            x = int(x.replace(')/',''))
+            x = x.replace('/Date(', '')
+            x = int(x.replace(')/', ''))
             return datetime.fromtimestamp(x/1000).strftime('%Y-%m-%d')
-        
+
         def GetResultsJson(num):
-            data = json.dumps({"numbers":[str(num).zfill(4)], "checkCombinations":"true", "sortTypeInteger":"1"})
+            data = json.dumps({"numbers": [str(num).zfill(4)], "checkCombinations": "true", "sortTypeInteger": "1"})
             r = requests.post(url=url, data=data, headers=headers)
             ResultsData = json.loads(r.json().get('d'))[0].get('Prizes')
-            Results_df = pd.DataFrame.from_dict(ResultsData) # dict to DF
-            Results_df["DrawDate"] = Results_df["DrawDate"].apply(getDateFromDrawDate,1)
+            Results_df = pd.DataFrame.from_dict(ResultsData)  # dict to DF
+            Results_df["DrawDate"] = Results_df["DrawDate"].apply(getDateFromDrawDate, 1)
             Results_df["Digit"] = str(num).zfill(4)
-            
+
             # Line chart
             dateList = Results_df['DrawDate'].values.tolist()
             prizeCodeList = Results_df['PrizeCode'].values.tolist()
@@ -51,36 +50,21 @@ def run_Scraping(scrapeAllNumbers, numberList):
                 'prizeCode': prizeCodeList
             })
             lineChartDF = lineChartDF.set_index('date')
-            st.line_chart(lineChartDF, use_container_width=True)
-            
+            if showGraph:    
+                st.success(num)
+                st.line_chart(lineChartDF, use_container_width=True)
+
             return Results_df
 
-        my_bar = st.progress(0)
-        percent_complete = 0
         ResultsAll = pd.DataFrame()
-        if scrapeAllNumbers:
-            for i in range(0, 10000):
-                ResultsData = None
-                while ResultsData is None:
-                    try:
-                        ResultsData = GetResultsJson(i)
-                    except:
-                        pass
-                ResultsAll = ResultsAll.append(ResultsData)
-                if (i % 100 == 0):  
-                    time.sleep(0.1)
-                    percent_complete += 1
-                    my_bar.progress(percent_complete)
-        else: 
-            for i in numberList:
-                ResultsData = None
-                while ResultsData is None:
-                    try:
-                        st.success(i)
-                        ResultsData = GetResultsJson(i)
-                    except:
-                        pass
-                ResultsAll = ResultsAll.append(ResultsData)
+        for i in numberList:
+            ResultsData = None
+            while ResultsData is None:
+                try:
+                    ResultsData = GetResultsJson(i)
+                except:
+                    pass
+            ResultsAll = ResultsAll.append(ResultsData)
         
         st.dataframe(ResultsAll)
 
@@ -91,7 +75,8 @@ def run_Scraping(scrapeAllNumbers, numberList):
             'prizeCode': prizeCodeList
         })
         lineChartDF = lineChartDF.set_index('date')
-        st.line_chart(lineChartDF, use_container_width=True)
+        lineChartDF.sort_values(by=['date'], inplace=True, ascending=False)
+        st.line_chart(lineChartDF.head(15) , use_container_width=True)
 
         if st.button('Download 4D Data as CSV'):
             tmp_download_link = download_link(ResultsAll, '4D_Data.csv', 'Download Data as CSV')
